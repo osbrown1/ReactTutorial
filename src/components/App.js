478f85cd3105d8069f8e0600fd5dcc9ad1030ff7
osbrown1 <socknot@live.com>
2023-04-18 09:53:12 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 function App(){
   return ( 
     <React.Fragment>
       <Header />
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
 
 export default App;
-
 
 
 // import logo from './logo.svg';
